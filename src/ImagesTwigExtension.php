5fa2494963d02a25420ce0d86f5acc19065f8ee1
Daniel Metzler <daniel.metzler@massiveart.com>
2018-03-27 09:44:10 +0200

    modified code after review

--- src/ImagesTwigExtension.php
+++ src/ImagesTwigExtension.php
@@ -34,53 +34,43 @@ class ImagesTwigExtension extends \Twig_Extension
      *     classes: 'image-class',          --> Set classes for image tag (type: string).
      * } %}
      *
-     * @param object $image - Contains the image object from sulu.
+     * @param Media $image - Contains the image object from sulu.
      * @param string $width - Contains the image format ('500x400').
      * @param array $options - Includes the attributes for the image element (id, classes, retinaSizes, alt).
      *
      * @return string
      */
-    public function getFixedImage($image, $width = '', $options = []) : string
+    public function getFixedImage(Media $image, $width, $options = array())
     {
         // Return an empty string if no one of the needed parameters is set.
-        if (empty($image) || empty($width)) {
+        if (empty($width) || empty($image->getThumbnails()[$width])) {
             return '';
         }
 
-        // Create the image tag.
+        // Open the image tag.
         $imageHtml = '<img';
 
         // Add an id to the image if it was set in options.
-        if (array_key_exists('id', $options) && !empty($options['id'])) {
-            $imageHtml .= ' id="' . $options['id'] . '"';
-        }
+        $imageHtml .= $this->getIdHtml($options);
 
         // Add classes to the image if it was set in options.
-        if (array_key_exists('classes', $options) && !empty($options['classes'])) {
-            $imageHtml .= ' class="' . $options['classes'] . '"';
-        }
+        $imageHtml .= $this->getClassHtml($options);
 
         // Add the image source as a thumbnail to the image.
         $imageHtml .= ' src="' . $image->getThumbnails()[$width] . '"';
 
         // Add the srcset to the image if it was set in the options.
         if (array_key_exists('retinaSizes', $options) && !empty($options['retinaSizes'])) {
-            $srcSet = [];
+            $srcSet = array();
             foreach ($options['retinaSizes'] as $key => $value) {
-                $srcSet[$key] = $image->getThumbnails()[$key] . ' ' . $value;
+                $srcSet[] = $image->getThumbnails()[$key] . ' ' . $value;
             }
 
             $imageHtml .= ' srcset="' . implode(', ', $srcSet) . '"';
         }
 
-        // Check if the alt attribute is set in the options, else take the default one.
-        $imageTitle = $image->getTitle();
-        if (array_key_exists('alt', $options) && !empty($options['alt'])) {
-            $imageTitle = $options['alt'];
-        }
-
         // Add the alt attribute to the image.
-        $imageHtml .= ' alt="' . $imageTitle . '"';
+        $imageHtml .= $this->getAltHtml($image->getTitle(), $options);
 
         // Close the image tag.
         $imageHtml .= '/>';
@@ -92,7 +82,7 @@ class ImagesTwigExtension extends \Twig_Extension
      * Get a responsive image html.
      *
      * Example in TWIG:
-     * {% set options2 = {
+     * {% set options = {
      *     fallBackImageFormat: 'sulu-400x400',     --> Set the fallback format for image if srcset doesn't exist (type: string).
      *     srcsetWidths: {                          --> Set the image format and the width for this image (type: array with string).
      *         'sulu-400x400': '1024w',
@@ -109,30 +99,21 @@ class ImagesTwigExtension extends \Twig_Extension
      *     classes: 'image-class',                  --> Set classes for image tag (type: string).
      * } %}
      *
-     * @param object $image
+     * @param Media $image
      * @param array $options
      *
      * @return string
      */
-    public function getResponsiveImage($image, $options = [])
+    public function getResponsiveImage(Media $image, $options)
     {
-        // Return an empty string if no one of the needed parameters is set.
-        if (empty($image)) {
-            return '';
-        }
-
-        // Create the image tag.
+        // Open the image tag.
         $imageHtml = '<img';
 
         // Add an id to the image if it was set in options.
-        if (array_key_exists('id', $options) && !empty($options['id'])) {
-            $imageHtml .= ' id="' . $options['id'] . '"';
-        }
+        $imageHtml .= $this->getIdHtml($options);
 
         // Add classes to the image if it was set in options.
-        if (array_key_exists('classes', $options) && !empty($options['classes'])) {
-            $imageHtml .= ' class="' . $options['classes'] . '"';
-        }
+        $imageHtml .= $this->getClassHtml($options);
 
         // Add the image source as a thumbnail to the image.
         if (array_key_exists('fallBackImageFormat', $options) && !empty($options['fallBackImageFormat'])) {
@@ -146,7 +127,7 @@ class ImagesTwigExtension extends \Twig_Extension
 
         // Add the srcset to the image if it was set in the options.
         if (array_key_exists('srcsetWidths', $options) && !empty($options['srcsetWidths'])) {
-            $srcSet = [];
+            $srcSet = array();
             foreach ($options['srcsetWidths'] as $key => $value) {
                 $srcSet[$key] = $image->getThumbnails()[$key] . ' ' . $value;
             }
@@ -154,14 +135,8 @@ class ImagesTwigExtension extends \Twig_Extension
             $imageHtml .= ' srcset="' . implode(', ', $srcSet) . '"';
         }
 
-        // Check if the alt attribute is set in the options, else take the default one.
-        $imageTitle = $image->getTitle();
-        if (array_key_exists('alt', $options) && !empty($options['alt'])) {
-            $imageTitle = $options['alt'];
-        }
-
         // Add the alt attribute to the image.
-        $imageHtml .= ' alt="' . $imageTitle . '"';
+        $imageHtml .= $this->getAltHtml($image->getTitle(), $options);
 
         // Close the image tag.
         $imageHtml .= '/>';
@@ -173,39 +148,34 @@ class ImagesTwigExtension extends \Twig_Extension
      * Get a responsive picture html.
      *
      * Example in TWIG:
-     * {% set options4 = {
-     *     fallBackImageFormat: 'sulu-400x400',     --> Set the fallback format for image if srcset doesn't exist (type: string).
-     *     imageFormats: [                          --> Set the image formats for this image (type: array with string).
+     * {% set options = {
+     *     fallBackImageFormat: 'sulu-400x400',
+     *     imageFormats: [
      *         'sulu-400x400',
      *         'sulu-170x170',
      *         'sulu-100x100'
      *     ],
-     *     medias: [                                --> Set the medias for source attribute (type: array).
+     *     medias: [
      *         '(max-width: 1024px)',
      *         '(max-width: 800px)'
      *     ],
-     *     retinaSizes: {                           --> Set the retina sizes you need for this image (type: array).
+     *     retinaSizes: {
      *         'sulu-400x400': '3x',
      *         'sulu-170x170': '2x',
      *         'sulu-100x100': '1x'
      *     },
-     *     alt: 'Logo',                             --> Set alt name for image tag (type: string).
-     *     id: 'image-id',                          --> Set id for image tag (type: string).
-     *     classes: 'image-class',                  --> Set classes for image tag (type: string).
+     *     alt: 'Logo',
+     *     id: 'image-id',
+     *     classes: 'image-class',
      * } %}
      *
-     * @param object $image
+     * @param Media $image
      * @param array $options
      *
      * @return string
      */
-    public function getResponsivePicture($image, $options)
+    public function getResponsivePicture(Media $image, $options)
     {
-        // Return an empty string if no one of the needed parameters is set.
-        if (empty($image)) {
-            return '';
-        }
-
         // Create the picture tag.
         $pictureHtml = '<picture>';
 
@@ -213,7 +183,7 @@ class ImagesTwigExtension extends \Twig_Extension
         if (array_key_exists('medias', $options) && !empty($options['medias'])
             && array_key_exists('retinaSizes', $options) && !empty($options['retinaSizes'])) {
             foreach ($options['medias'] as $key => $value) {
-                $srcSet = [];
+                $srcSet = array();
 
                 // Set a fallback image format if no retina size can be used.
                 $srcSet[] = $image->getThumbnails()[$options['imageFormats'][$key]];
@@ -232,28 +202,18 @@ class ImagesTwigExtension extends \Twig_Extension
         $pictureHtml .= '<img';
 
         // Add an id to the image if it was set in options.
-        if (array_key_exists('id', $options) && !empty($options['id'])) {
-            $pictureHtml .= ' id="' . $options['id'] . '"';
-        }
+        $pictureHtml .= $this->getIdHtml($options);
 
         // Add classes to the image if it was set in options.
-        if (array_key_exists('classes', $options) && !empty($options['classes'])) {
-            $pictureHtml .= ' class="' . $options['classes'] . '"';
-        }
+        $pictureHtml .= $this->getClassHtml($options);
 
         // Add the image source as a thumbnail to the image.
         if (array_key_exists('fallBackImageFormat', $options) && !empty($options['fallBackImageFormat'])) {
             $pictureHtml .= ' src="' . $image->getThumbnails()[$options['fallBackImageFormat']] . '"';
         }
 
-        // Check if the alt attribute is set in the options, else take the default one.
-        $imageTitle = $image->getTitle();
-        if (array_key_exists('alt', $options) && !empty($options['alt'])) {
-            $imageTitle = $options['alt'];
-        }
-
         // Add the alt attribute to the image.
-        $pictureHtml .= ' alt="' . $imageTitle . '"';
+        $pictureHtml .= $this->getAltHtml($image->getTitle(), $options);
 
         // Close the image tag.
         $pictureHtml .= '/>';
@@ -262,5 +222,50 @@ class ImagesTwigExtension extends \Twig_Extension
         $pictureHtml .= '</picture>';
 
         return $pictureHtml;
+    }
+
+    /**
+     * @param array $options
+     *
+     * @return string
+     */
+    protected function getIdHtml($options)
+    {
+        if (array_key_exists('id', $options) && !empty($options['id'])) {
+            return ' id="' . $options['id'] . '"';
+        }
+
+        return '';
+    }
+
+    /**
+     * @param array $options
+     *
+     * @return string
+     */
+    protected function getClassHtml($options)
+    {
+        if (array_key_exists('classes', $options) && !empty($options['classes'])) {
+            return ' class="' . $options['classes'] . '"';
+        }
+
+        return '';
+    }
+
+    /**
+     * @param string $imageTitle
+     * @param array $options
+     *
+     * @return string
+     */
+    protected function getAltHtml($imageTitle, $options)
+    {
+        // Check if the alt attribute is set in the options, else take the default one.
+        $title = $imageTitle;
+        if (array_key_exists('alt', $options) && !empty($options['alt'])) {
+            $title = $options['alt'];
+        }
+
+        return ' alt="' . $title . '"';
     }
 }
